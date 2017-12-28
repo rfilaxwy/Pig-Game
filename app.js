c0e728afcf38ceeea9fcff1f323a385b17bd3db0
Ryan Filax-Wylie <ryanfilaxwylie@gmail.com>
2017-12-28 07:08:51 -0700

    Completed JS for pig game

--- app.js
+++ app.js
@@ -9,56 +9,106 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer;
-scores =[0,0];
-roundScore = 0;
-activePlayer =0;
+var scores, roundScore, activePlayer, gamePlaying;
+	init();
 
-
-
-
-
-document.querySelector('.dice').style.display='none';
+function init() {
+	scores= [0,0];
+	activePlayer = 0;
+	roundScore = 0;
+	gamePlaying =true;
+	document.querySelector('.dice').style.display='none';
 
 document.getElementById('score-0').textContent='0';
 document.getElementById('score-1').textContent='0';
 document.getElementById('current-0').textContent='0';
 document.getElementById('current-1').textContent='0';
+document.getElementById('name-0').innerHTML = 'Player 1';
+document.getElementById('name-1').innerHTML = 'Player 2';
+document.querySelector('.player-0-panel').classList.remove('winner');
+document.querySelector('.player-1-panel').classList.remove('winner');
+document.querySelector('.player-0-panel').classList.remove('active');
+document.querySelector('.player-1-panel').classList.remove('active');
+document.querySelector('.player-0-panel').classList.add('active');
+
+
+};
+
+
+
+
 
 
 
 document.querySelector('.btn-roll').addEventListener('click', function(){
-	//1.Random number
-	var dice=Math.floor(Math.random()*6)+1;
-	//2.Display the result
-	var diceDOM = document.querySelector('.dice');
-	diceDOM.style.display ='block';
-	diceDOM.src = 'dice-'+dice+'.png';
-	document.querySelector('#current-'+activePlayer).innerHTML= dice;
-	//3.Update the round score only if the rolled number was not 1
-	if (dice!==1){
-		//add score
-		roundScore+=dice;
-		document.querySelector('#current-'+activePlayer).textContent = roundScore;
-	} else {
-		document.querySelector('#current-'+activePlayer).innerHTML= 0;
-		roundScore=0;
-		if (activePlayer  ===1){
-			activePlayer =0;
-			} else {
-				activePlayer = 1;
+	if (gamePlaying){
+		//1.Random number
+		var dice=Math.floor(Math.random()*6)+1;
+		//2.Display the result
+		var diceDOM = document.querySelector('.dice');
+		diceDOM.style.display ='block';
+		diceDOM.src = 'dice-'+dice+'.png';
+		document.querySelector('#current-'+activePlayer).innerHTML= dice;
+		//3.Update the round score only if the rolled number was not 1
+
+		if (dice!==1) {
+			//add score
+			roundScore+=dice;
+			document.querySelector('#current-'+activePlayer).textContent = roundScore;					
+		} else {
+			//Next Player
+			nextPlayer();
+
+
+			//document.querySelector('.player-0-panel').classList.remove('active');
+			//document.querySelector('.player-1-panel').classList.add('active');
+
 			}
-		}
+	}
+				
 });
 
+	document.querySelector('.btn-hold').addEventListener('click', function(){
+		//Add player current score to global score
+		scores[activePlayer] += roundScore;
+		
 
+		//Update the UI
+		document.querySelector('#score-'+activePlayer).textContent =scores[activePlayer];
 
+		//Check if the player won the game
+		if (scores[activePlayer]>=10){
+			document.querySelector('#name-'+activePlayer).textContent = 'Winner!';
+			document.querySelector('.dice').style.display = 'none';
+			document.querySelector('.player-'+activePlayer+'-panel').classList.add('winner');
+			document.querySelector('.player-'+activePlayer+'-panel').classList.remove('active');
+			gamePlaying=false;
+		} else {
+			//Next Player
+		nextPlayer();
+		}
 
+	});
 
+	function nextPlayer() {
+		document.querySelector('#current-'+activePlayer).innerHTML= 0;
+		roundScore=0;
+		activePlayer === 0 ? activePlayer=1 : activePlayer = 0;
 
+		document.getElementById('current-0').textContent = '0';
+		document.getElementById('current-1').textContent = '0';
+		
+		document.querySelector('.player-0-panel').classList.toggle('active');
+		document.querySelector('.player-1-panel').classList.toggle('active');
 
+		document.querySelector('.dice').style.display ='none';
+	};
 
 
 //document.querySelector('#current-'+activePlayer).textContent = dice;
 //document.querySelector('#current-'+activePlayer).innerHTML ='<em>'+dice+'</em>';
 //var x = document.querySelector('#score-0').textContent;
+
+//Add new game button functionality
+document.querySelector('.btn-new').addEventListener('click', init);
+
